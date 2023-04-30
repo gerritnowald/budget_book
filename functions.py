9a090eed19e715e52ed8acfee138b6c3093a64de
Gerrit Nowald <gerrit.nowald@gmail.com>
2023-04-30 11:29:02 +0200

    modified table, new plots

--- functions.py
+++ functions.py
@@ -24,37 +24,38 @@ def get_sum_cat_month(transactions, clm, next_months=3):
     sum_cat_month = sum_cat_month.transpose()
     
     
-    # columns sum & mean/month per category
-    sum_cat_month[clm['sum']] , sum_cat_month[clm['mean_month']] = sum_cat_month.sum(axis=1) , sum_cat_month.mean(axis=1)   # mean not influenced by sum
-    
-    # column mean/transaction per category
-    sum_cat_month[clm['mean_trsct']] = sum_cat_month[clm['sum']] / transactions_cat.count()[clm['amount']]
-    
+    # column sum per category
+    sum_cat_month[clm['sum']] = sum_cat_month.sum(axis=1)
+
+    # column mean/month (excluding current month)
+    sum_cat_month[clm['mean_month']] = sum_cat_month.iloc[:,:-2].sum(axis=1)  / ( sum_cat_month.shape[1] - 2 )
+
     
     sum_cat_month = sum_cat_month.sort_index()  # by category
-    # sum_cat_month = sum_cat_month.sort_values(clm['sum'])
+    # sum_cat_month = sum_cat_month.sort_values(clm['sum'])  # by sum
     
     
     # forecast for next months
-    forecast = sum_cat_month[clm['sum']] / ( sum_cat_month.shape[1] - 3 )
-    
-    next_month_lst = [ max(transactions[clm['date']]) ]
-    for _ in range(next_months):
-        next_month_lst.append( max(next_month_lst) + relativedelta.relativedelta(months=1) )
-    next_month_lst = next_month_lst[1:]
-    next_month_lst = [ str(month)[:7] for month in next_month_lst ]    # prettier index
-    
-    NEXT_MONTHS    = np.array(forecast)[:,np.newaxis] * np.ones(( sum_cat_month.shape[0] , next_months ))
-    NEXT_MONTHS_df = pd.DataFrame( NEXT_MONTHS , columns = next_month_lst , index = sum_cat_month.index  )
-    sum_cat_month  = pd.concat( [ sum_cat_month , NEXT_MONTHS_df ] , axis=1 )
-    
-    
-    # row sum per month
-    sum_cat_month = pd.concat([ sum_cat_month , sum_cat_month.sum().rename(clm['sum']).to_frame().T ])   # after sorting
+    if next_months > 0:
+        forecast = sum_cat_month[clm['mean_month']]
+        
+        next_month_lst = [ max(transactions[clm['date']]) ]
+        for _ in range(next_months):
+            next_month_lst.append( max(next_month_lst) + relativedelta.relativedelta(months=1) )
+        next_month_lst = next_month_lst[1:]
+        next_month_lst = [ str(month)[:7] for month in next_month_lst ]    # prettier index
+        
+        NEXT_MONTHS    = np.array(forecast)[:,np.newaxis] * np.ones(( sum_cat_month.shape[0] , next_months ))
+        NEXT_MONTHS_df = pd.DataFrame( NEXT_MONTHS , columns = next_month_lst , index = sum_cat_month.index  )
+        sum_cat_month  = pd.concat( [ sum_cat_month , NEXT_MONTHS_df ] , axis=1 )
     
     
-    sum_cat_month[clm['mean_month']][clm['sum']] = forecast.sum()
-    sum_cat_month[clm['mean_trsct']][clm['sum']] = np.nan
+    # row expenses (sum of negatives)
+    sum_cat_month = pd.concat([ sum_cat_month , sum_cat_month[sum_cat_month<0].sum().rename(clm['expenses']).to_frame().T ])   # after sorting
+    
+    # row balance per month
+    sum_cat_month = pd.concat([ sum_cat_month , sum_cat_month.drop(clm['expenses'],axis=0).sum().rename(clm['sum']).to_frame().T ])
+    
     
     sum_cat_month = sum_cat_month.round()
     
