467bd4f64f6acbd80b54dcc9611bc1ff1acda510
Gerrit Nowald <gerrit.nowald@gmail.com>
2026-02-07 12:29:59 +0100

    simplified dataframe creation fixed sums due to reversed transaction order

--- src/analysis_functions.py
+++ src/analysis_functions.py
@@ -4,69 +4,49 @@
 """
 
 import pandas as pd
-import numpy as np
-
-# -----------------------------------------------------------------------------
-# table operations
 
 def get_sum_cat_month(transactions, clm):
     """creates a table of spendings per month and category"""
 
     # group by month and category
     sum_cat_month = ( transactions
-                        .groupby(clm['category'])
-                        .resample('1ME', on=clm['date'])
-                        .sum(numeric_only=True)[clm['amount']]
-                        .unstack(0) )
+        .groupby([ clm['category'] ,
+            pd.Grouper(key=clm['date'], freq='ME') ])[clm['amount']]
+        .sum().unstack(level=0, fill_value=0).T )
     
-    # prettier index
-    sum_cat_month.index = sum_cat_month.index.map(lambda date : str(date)[:7])
+    # prettier column titles
+    sum_cat_month.columns = sum_cat_month.columns.strftime('%Y-%m')
 
-    sum_cat_month = sum_cat_month.transpose()
-
-    # split categories
-    sum_cat_month[ [ clm['category'] , clm['cat_fine'] ] ] = sum_cat_month.index.to_series().str.split('/', expand=True)
-    sum_cat_month[clm['cat_fine']] = sum_cat_month[clm['cat_fine']].replace(np.nan, '')
+    # split categories into a MultiIndex
+    sum_cat_month[ [ clm['category'] , clm['cat_fine'] ] ] = sum_cat_month.index.to_series().str.split('/', expand=True).fillna('')
     sum_cat_month = sum_cat_month.set_index([ clm['category'] , clm['cat_fine'] ])
     
 
-    # column sum per category
-    sum_cat_month[clm['sum']] = sum_cat_month.sum(axis=1)
+    # dataframes for plots
+    df_sunburst = sum_cat_month.sum(axis=1)
+    df_sunburst.name = clm['sum']
+    df_sunburst = df_sunburst[ df_sunburst < 0].abs().reset_index()
 
-    # column mean/month (excluding current month)
-    sum_cat_month[clm['mean_month']] = sum_cat_month.iloc[:,:-2].sum(axis=1)  / ( sum_cat_month.shape[1] - 2 )
+    df_plot_rough = sum_cat_month.groupby(level=0).sum().T
 
     
+    # additional rows
+    balance = sum_cat_month.sum()
+    sum_cat_month.loc[(clm['expenses'  ], ''), :] = sum_cat_month[sum_cat_month < 0].sum()
+    sum_cat_month.loc[(clm['sum'       ], ''), :] = balance
+
+    
+    # dataframe for plots
+    df_plot_fine = sum_cat_month.T
+
+
+    # additional columns
+    sum_cat_month[clm['sum']] = sum_cat_month.sum(axis=1)   # sum per category
+    sum_cat_month[clm['mean_month']] = (sum_cat_month.iloc[:,:-2].sum(axis=1)  / 
+            ( sum_cat_month.shape[1] - 2 )).round(2)  # mean/month (excluding current month)
+
+
     # sum_cat_month = sum_cat_month.sort_index()  # by category
     # sum_cat_month = sum_cat_month.sort_values(clm['sum'], ascending=False)  # by sum
-    
-    
-    # row expenses (sum of negatives)
-    expenses = pd.DataFrame( [sum_cat_month[sum_cat_month < 0].sum()] , 
-            columns = sum_cat_month.columns , index = [(clm['expenses'], '')] )
-    
-    # row balance per month
-    balance = pd.DataFrame( [sum_cat_month.sum()] , 
-            columns = sum_cat_month.columns , index = [(clm['sum'], '')] )
-
-    sum_cat_month = pd.concat([ sum_cat_month, expenses, balance ])
-
-
-    sum_cat_month.index = sum_cat_month.index.rename([clm['category'],clm['cat_fine']])
-    
-    return sum_cat_month.round().replace(0, np.nan)
-
-
-
-def df_plots(sum_cat_month, clm):
-    """reformats the table for plotting"""
-    df_plot_fine = sum_cat_month.T.drop([ clm['sum'], clm['mean_month'] ])
-
-    df_plot_rough = df_plot_fine.T.drop([ clm['expenses'], clm['sum'] ], level=0)
-    df_plot_rough = df_plot_rough.groupby(clm['category']).sum().T
-
-    df_sunburst = sum_cat_month[[clm['sum']]].drop([ clm['expenses'], clm['sum'] ], level=0).reset_index()
-    df_sunburst = df_sunburst[ df_sunburst[clm['sum']] < 0]
-    df_sunburst[clm['sum']] = df_sunburst[clm['sum']].abs()
-
-    return df_plot_fine, df_plot_rough, df_sunburst
+        
+    return sum_cat_month, df_plot_fine, df_plot_rough, df_sunburst
